b1f3e961718fa5716164c1c8bb0a2e3b92f475d8
Mark Chmarny <mark@chmarny.com>
2019-04-16 15:14:11 -0700

    Cleaned up commands

--- HYB218/cmd/deploy-klogo.sh
+++ HYB218/cmd/deploy-klogo.sh
@@ -1,3 +1,3 @@
 gcloud beta run deploy klogo \
     --image=gcr.io/s9-demo/klogo@sha256:c91c08d92323d8d9e2b6c899249dde343ed61997db62d58deadb946b6365663d \
-    --set-env-vars=RELEASE=v0.0.5-demo,GCP_PROJECT_ID=s9-demo,GIN_MODE=release
+    --set-env-vars=RELEASE=v0.0.5-demo,GCP_PROJECT_ID=$GCLOUD_PROJECT,GIN_MODE=release
